43efe31ce86edef5d7854e59e057bb4b0b6c7e00
nvlight <lightstyle11@mail.ru>
2022-04-03 15:15:47 +0300

    change welcome #2

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel again
+                    Laravel again - with branch dev
                 </div>
 
                 <div class="links">
